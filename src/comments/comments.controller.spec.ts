02c1b86d147b54a715a518bac5ebe20c19c25757
Carlos Forti <carlosforti@carlosforti.com.br>
2019-08-27 16:01:57 -0300

    Ajustando specs

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
+import { Comment } from './comment.entity';
+import { Repository } from 'typeorm';
 
 describe('Comments Controller', () => {
   let controller: CommentsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CommentsController],
-    }).compile();
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     controllers: [CommentsController],
+  //   }).compile();
 
-    controller = module.get<CommentsController>(CommentsController);
+  //   controller = module.get<CommentsController>(CommentsController);
+  // });
+
+  beforeEach(() => {
+    controller = new CommentsController(new CommentsService(new Repository<Comment>()));
   });
 
   it('should be defined', () => {
--- src/comments/comments.service.spec.ts
+++ src/comments/comments.service.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsService } from './comments.service';
+import { Repository } from 'typeorm';
+import { Comment } from './comment.entity';
 
 describe('CommentsService', () => {
   let service: CommentsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentsService],
-    }).compile();
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     providers: [CommentsService],
+  //   }).compile();
 
-    service = module.get<CommentsService>(CommentsService);
+  //   service = module.get<CommentsService>(CommentsService);
+  // });
+
+  beforeEach(async () => {
+    service = new CommentsService(new Repository<Comment>());
   });
 
   it('should be defined', () => {
